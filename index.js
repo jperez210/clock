429ba3faa333b92703c53348173f27fe0abb52b7
Jacob <perezjacob2@gmail.com>
2021-02-26 20:11:18 -0600

    Clock hands and styling
    Added function to getTime to get the current time.
    
    Added CSS for background and clock hands and face.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const hourHand = document.querySelector('.hand-hour');
 const minuteHand = document.querySelector('.hand-minute');
 const secondHand = document.querySelector('.hand-second');
+
+function getTime() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondDegree = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`
+
+    const minutes = now.getMinutes();
+    const minuteDegree = ((minutes / 60) *360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`
+
+    const hours = now.getHours();
+    const hoursDegree = ((hours / 60)  *360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`
+
+}
+
+setInterval (getTime, 1000);
